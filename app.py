3cd8a8a1ff3b494cc0d03f115bc34221b8d1161c
Mark Buckle <markbuckle92@gmail.com>
2024-11-17 16:59:39 -0330

    Split text into chunks

--- app.py
+++ app.py
@@ -1,18 +1,51 @@
 import streamlit as st
-from dotenv import load_dotenv # enables app to use .env
+from dotenv import load_dotenv  # enables app to use .env
+from PyPDF2 import PdfReader
+from langchain.text_splitters import CharacterTextSplitter
+
+
+def get_pdf_text(pdf_docs):
+    text = ""  # contains raw text of pdfs
+    for pdf in pdf_docs:  # loop though pdfs
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:  # read each page
+            text += page.extract_text()  # extract / concatenate useful text
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
-    
+
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask question about your documents:")
-    
+
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True
+        )
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                # get text chunks (to feed to the database)
+                text_chunks = get_text_chunks(raw_text)
+                # st. write(text_chunks)
+
+                # create vector store (database)
+
 
 # run only if the application is being run directly and not imported
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
